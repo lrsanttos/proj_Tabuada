d9974143254df5150fc8d5d4ea6c501ee893c429
Leandro Ribeiro <161388759+lrsanttos@users.noreply.github.com>
2024-02-26 21:46:07 -0300

    Terceiro commit
    Add código-fonte.

---
+++ ProjetoLaco/src/ClassePrincipal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		
+		while(true){
+			System.out.println("Informe a calculadora: ");
+			Scanner sc = new Scanner(System.in);
+			int num = sc.nextInt();	
+			
+			for(int i=0; i<=10; i++){
+				System.out.printf(num + " * " + i + " = ");
+				System.out.println(num * i);
+			}
+			break;
+		}
+	}
+}
